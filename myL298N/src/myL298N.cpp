6fdda1849fed6bdf0c41131ca45f3897bc49d45c
hoanganh2k2 <anhkd2002@gmail.com>
2025-07-17 16:58:12 +0700

    Update PWM

--- myL298N/src/myL298N.cpp
+++ myL298N/src/myL298N.cpp
@@ -1,45 +1,49 @@
 #include "myL298N.h"
 #include <Arduino.h>
 
-myL298N::myL298N(int in1, int in2, int in3, int in4)
-    : _in1(in1), _in2(in2), _in3(in3), _in4(in4) {}
+myL298N::myL298N(int in1, int in2, int in3, int in4, int ena, int enb)
+    : _in1(in1), _in2(in2), _in3(in3), _in4(in4), _ena(ena), _enb(enb) {}
 
 void myL298N::begin() {
     pinMode(_in1, OUTPUT);
     pinMode(_in2, OUTPUT);
     pinMode(_in3, OUTPUT);
     pinMode(_in4, OUTPUT);
+    pinMode(_ena, OUTPUT);
+    pinMode(_enb, OUTPUT);
     stop();
 }
 
-void myL298N::setMotorL(bool forward) {
+void myL298N::setMotorL(bool forward, int speed) {
     digitalWrite(_in1, forward ? HIGH : LOW);
     digitalWrite(_in2, forward ? LOW : HIGH);
+    analogWrite(_ena, speed);
 }
 
-void myL298N::setMotorR(bool forward) {
+void myL298N::setMotorR(bool forward, int speed) {
     digitalWrite(_in3, forward ? HIGH : LOW);
     digitalWrite(_in4, forward ? LOW : HIGH);
+    analogWrite(_enb, speed);
 }
 
-void myL298N::forward() {
-    setMotorL(true);
-    setMotorR(true);
+void myL298N::forward(int speed) {
+    setMotorL(true, speed);
+    setMotorR(true, speed);
 }
 
-void myL298N::backward() {
-    setMotorL(false);
-    setMotorR(false);
+void myL298N::backward(int speed) {
+    setMotorL(false, speed);
+    setMotorR(false, speed);
 }
 
-void myL298N::turnright() {
-    setMotorL(true);
-    setMotorR(false);
+void myL298N::turnright(int speed) {
+    setMotorL(true, speed);
+    setMotorR(false, speed);
 }
 
-void myL298N::turnleft() {
-    setMotorL(false);
-    setMotorR(true);
+void myL298N::turnleft(int speed) {
+    setMotorL(false, speed);
+    setMotorR(true, speed);
 }
 
 void myL298N::brake() {
@@ -47,6 +51,8 @@ void myL298N::brake() {
     digitalWrite(_in2, HIGH);
     digitalWrite(_in3, HIGH);
     digitalWrite(_in4, HIGH);
+    analogWrite(_ena, 0);
+    analogWrite(_enb, 0);
 }
 
 void myL298N::stop() {
@@ -54,4 +60,6 @@ void myL298N::stop() {
     digitalWrite(_in2, LOW);
     digitalWrite(_in3, LOW);
     digitalWrite(_in4, LOW);
+    analogWrite(_ena, 0);
+    analogWrite(_enb, 0);
 }
--- myL298N/src/myL298N.h
+++ myL298N/src/myL298N.h
@@ -3,18 +3,19 @@
 
 class myL298N {
 public:
-    myL298N(int in1, int in2, int in3, int in4);
+    myL298N(int in1, int in2, int in3, int in4, int ena, int enb);
     void begin();
-    void forward();
-    void backward();
-    void turnright();
-    void turnleft();
+    void forward(int speed = 255);
+    void backward(int speed = 255);
+    void turnright(int speed = 255);
+    void turnleft(int speed = 255);
     void brake();
     void stop();
-    void setMotorR(bool forward);
-    void setMotorL(bool forward);
+    void setMotorL(bool forward, int speed = 255);
+    void setMotorR(bool forward, int speed = 255);
 private:
     int _in1, _in2, _in3, _in4;
+    int _ena, _enb;
 };
 
 #endif
